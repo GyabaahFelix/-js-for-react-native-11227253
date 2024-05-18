05577f016c290b7d3eb8e8d8370d5713e48ea66d
Gyabaah Felix <fgyabaah003@st.ug.edu.gh>
2024-05-18 14:54:59 +0000

    Add files via upload

---
+++ first.js
@@ -0,0 +1,9 @@
+// Example usage in another file
+
+const { processArray } = require('./arrayManipulation');
+
+const numbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(numbers);
+
+console.log(processedNumbers); // Output: [3, 4, 9, 16, 15]
+
